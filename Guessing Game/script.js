bd94fd0ca14be236479476e6779633bd1a1d48cc
MohammedAB <albay.m@hotmail.com>
2023-11-10 03:38:58 +0100

    Add new variables to manipulate + fix 'hintsOutput' and 'maxNum' value

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,12 +1,14 @@
 // TODO: Declare some variables to use in our game
 let minNum = 0;
-let maxNum = 26;
+let maxNum = 27;
 let randomNumber = Math.floor(Math.random()*(maxNum - minNum)+1)+ minNum;
 console.log(randomNumber);
 
 let attempts = 0;
 const guessInput = document.getElementById("Guess");
 console.log(guessInput);
+const startVersion = document.getElementById("game-box");
 const submitButton = document.getElementById("submitGuess");
-const hintsOutput = document.getElementById("hint");
+const restartButton = document.getElementById("restart");
+const hintsOutput = document.getElementById("hints");
 const attemptsOutput = document.getElementById("attempts");
