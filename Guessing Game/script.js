aed5feba1d73ddd2dc203bce15f636e83cdcbc9e
MohammedAB <albay.m@hotmail.com>
2023-11-10 03:46:47 +0100

    Define conditionals for checkGuess function

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -18,4 +18,21 @@ function checkGuess() {
   const guessValue = Number(guessInput.value);
   attempts++;
   console.log(attempts);
+  // Define conditionals
+  if (guessValue <= minNum || guessValue >= maxNum){
+    hintsOutput.textContent = `Enter a number between ${minNum} and ${maxNum} pls!`
+    hintsOutput.classList.add("error");
+  } else if (guessValue === randomNumber){
+    hintsOutput.textContent = `Awesome! Your number ${guessValue} was correct. You can be named many things, hungry not being one of them.`;
+    hintsOutput.classList.remove("error");
+    hintsOutput.classList.add("success");
+    submitButton.style.display = "none";
+    restartButton.style.display = "block";
+  } else if ((guessValue+1) === randomNumber || (guessValue-1) === randomNumber){
+    hintsOutput.textContent = `So close, but you just missed it! Are you in a class that started on the thirteenth or what?"`;
+    hintsOutput.classList.add("error");
+  } else {
+    hintsOutput.textContent = `Bummer...You guessed ${guessValue} and the secret number was ${randomNumber}.`
+    hintsOutput.classList.add("error");
+  }
 }
